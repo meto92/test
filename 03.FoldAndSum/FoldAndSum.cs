1ec4ec2db1e4a73bd9c4ba4b68ea303972732e5d
MeTo <m_popstoilov@abv.bg>
2017-11-14 14:34:30 +0200

    Added FoldAndSum

---
+++ 03.FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class FoldAndSum
+{
+    static void Main(string[] args)
+    {
+        long[] nums = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
+
+        int k = nums.Length / 4;
+        
+        for (int i = 0; i < k; i++)
+        {
+            int leftIndex = 2 * k - i - 1,
+                rightIndex = nums.Length - 2 * k + i;
+
+            nums[leftIndex] += nums[i];
+            nums[rightIndex] += nums[nums.Length - i - 1];
+        }
+
+        Console.WriteLine(string.Join(" ", nums.Skip(k).Take(2 * k)));
+    }
+}
